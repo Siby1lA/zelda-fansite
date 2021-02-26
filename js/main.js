12a92098e28854ea7f1cd49fa902884481975cbd
Subin <psb4644@gmail.com>
2021-02-26 20:08:02 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
+const btnVideo = document.querySelector(".trailer-video");
+const trailerVideo = document.querySelector(".trailer-movies");
+
+
+function onClick(e){
+    trailerVideo.style.display = 'flex';
+}
 
 
 function init(){
-
+    btnVideo.addEventListener('click', onClick);
 }
 
 init();
