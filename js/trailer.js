5559991d3d4c800987cf0c12352ce989475e7dc2
Subin <psb4644@gmail.com>
2021-02-26 21:17:08 +0900

    Created trailer delBtn

---
+++ js/trailer.js
@@ -0,0 +1,17 @@
+const btnVideo = document.querySelector(".trailer-video");
+const trailerVideo = document.querySelector(".trailer-movies");
+const delBtn = document.querySelector(".delBtn");
+
+function onClickVideo(e){
+    trailerVideo.style.display = 'flex';
+}
+function onClickDelBtn(e){
+    trailerVideo.style.display = 'none';
+}
+
+function init(){
+    btnVideo.addEventListener('click', onClickVideo);
+    delBtn.addEventListener('click', onClickDelBtn);
+}
+
+init();
